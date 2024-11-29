70f56cb0b7b02294ff8e102de501df506527eda6
ak6123 <aidankhan02@hotmail.com>
2024-11-29 22:07:05 +0000

    Program_Counter.sv V2

--- rtl/Program_Counter.sv
+++ rtl/Program_Counter.sv
@@ -1,18 +1,22 @@
-module program_counter #(
-    parameter PC_WIDTH = 32
-) (
+module program_counter #( //Model 2
+    parameter PC_WIDTH = 32 //using 32 bits, 32I
+) ( //external logic
     input   logic                   clk,
     input   logic                   rst,
     input   logic                   pc_src,
-    input   logic [PC_WIDTH-1:0]    pc_plusfour,
-    input   logic [PC_WIDTH-1:0]    pc_target,
+    input   logic [PC_WIDTH-1:0]    ImmExt, //new in log
     output  logic [PC_WIDTH-1:0]    pc
 );
-
+//internal logic
     logic [PC_WIDTH-1:0] pc_next;
+    logic [PC_WIDTH-1:0] pc_target; //made internal
+    logic [PC_WIDTH-1:0] pc_plus4;  //made internal //corr name
 
-    always_ff @ (posedge clk)//pos edge synch
-        pc_next = pc_src ? pc_target : pc_plusfour; //mux sel val is scr
+    assign pc_target = pc + ImmExt; //jmp or brch
+    assign pc_plus4 = pc + 4'b0100; //regular increment 
+    assign pc_next = pc_src ? pc_target : pc_plus4; //mux, sel val is scr  //corr name //doesn't meed posedge
+
+    always_ff @ (posedge clk) //pos edge synch
         if (rst)
             pc = 32'hBFC00000;  // INSTR MEM STARTS AT THIS VALUE
         else
