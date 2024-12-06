163fc1ad4d3f7c9bc81d81e210170c9c91b8d3fa
Darryl Boulton <darryl420boulton@gmail.com>
2024-12-06 18:04:28 +0000

    Small change, hazard unit complete by adding stalling on branch instructions.

--- rtl/hazard_unit.sv
+++ rtl/hazard_unit.sv
@@ -12,15 +12,22 @@ module hazard_unit #(
     output logic [1:0]              forward_a,      // Multiplexer Control Signals
     output logic [1:0]              forward_b,
 
-    // Stalls (for LW)
+    // Stalls
     input  logic [1:0]              result_src,
+    input  logic [4:0]              rd_out_id_ex,
+    input  logic [2:0]              branch_jump,
     output logic                    stall_f,
     output logic                    stall_d,
     output logic                    flush_e
 
 );
 
+    logic lw_stall;
+    logic branch_stall;
+    logic stall;
+
 always_comb begin
+    // Forwarding
     if ((rs1 == rd_m) & reg_write_m) & (rs1 != 0) begin             // Forward From Memory Access Stage (RS1)
         forward_a = 2'b10;
     end else if ((rs1 == rd_w) & reg_write_w) & (rs1 != 0) begin    // Forward From Writeback Stage (RS1)
@@ -37,9 +44,16 @@ always_comb begin
         forward_b = 2'b00;                                          // No Forwarding Required (RS2)
     end
 
+    // Stalling
+    lw_stall = (result_src == '0) & ((rs1 == rd_out_id_ex) || (rs2 == rd_out_id_ex));
+    stall_f = stall_d = flush_e = lw_stall;
 
-    
+    branch_stall = ~(branch_jump == 3'b010);
+
+    stall = branch_stall || lw_stall;
+
+    stall_f = stall_d = flush_e = stall;
+
 end
-
 
 endmodule
