30b71c7c065d01bd9a18b4d42e32d062e89112c1
Dawud Khan <dawudakhan0711@gmail.com>
2024-12-09 15:04:13 +0000

    pipelined-cpu control unit

--- rtl/control_unit.sv
+++ rtl/control_unit.sv
@@ -1,37 +1,41 @@
 module control_unit (
-    input  logic [31:0] instruction,
-    output logic       Reg_write, Mem_read, Mem_write, Branch,
-    output logic [4:0] ALU_ctrl,  // Adjusted for 5-bit ALU control
-    output logic [1:0] PCSrc,
-    output logic [2:0] ImmSrc
+    input   logic [31:0] instruction,
+    output  logic        reg_write_d,  
+    output  logic [1:0]  result_src_d,
+    output  logic        mem_write_d,
+    output  logic        jump_d,
+    output  logic        branch_d,
+    output  logic [2:0]  alu_control_d,
+    output  logic        alu_src_d,
+    output  logic [1:0]  imm_src_d
 );
 
+    // Extract instruction fields
     logic [6:0] opcode;
     logic [2:0] funct3;
     logic [6:0] funct7;
-    logic [1:0] ALU_Op;
-    logic [1:0] funct7_5_0; // New signal for the 5th and 0th bits of funct7
 
     assign opcode = instruction[6:0];
     assign funct3 = instruction[14:12];
     assign funct7 = instruction[31:25];
-    assign funct7_5_0 = {funct7[5], funct7[0]}; // Extract the required bits
 
-    main_decoder u_main_decoder (
+    // Main Decoder
+    main_decoder main_decoder_unit (
         .opcode(opcode),
-        .Reg_write(Reg_write), .Mem_read(Mem_read), .Mem_write(Mem_write),
-        .Branch(Branch), .ALU_Op(ALU_Op), .PCSrc(PCSrc), .ImmSrc(ImmSrc)
+        .reg_write_d(reg_write_d),
+        .result_src_d(result_src_d),
+        .mem_write_d(mem_write_d),
+        .jump_d(jump_d),
+        .branch_d(branch_d),
+        .alu_src_d(alu_src_d),
+        .imm_src_d(imm_src_d)
     );
 
-    alu_decoder #(
-        .FUNC3_WIDTH(3),
-        .ALUOP_WIDTH(2),
-        .ALUCTR_WIDTH(5)
-    ) u_alu_decoder (
-        .func3(funct3),
-        .alu_op(ALU_Op),
-        .func7_5_0(funct7_5_0),
-        .alu_ctrl(ALU_ctrl)
+    // ALU Decoder
+    alu_decoder alu_decoder_unit (
+        .funct3(funct3),
+        .funct7(funct7),
+        .alu_control_d(alu_control_d)
     );
 
 endmodule
